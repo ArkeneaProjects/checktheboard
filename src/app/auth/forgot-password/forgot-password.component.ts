f883b3414034fcea2d254874cb1db876d24f3493
guru <guru@arkenea.com>
2025-01-28 14:22:58 +0530

    forgot changes

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,5 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, inject, signal, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { validationMessages } from '../../core/utils/messages'
+import { AuthService } from '../../core/services/auth.service'
+import { CommonService } from '../../core/services/common.service'
+import { StepperOrientation,MatStepper } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-forgot-password', 
@@ -7,18 +12,93 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+  @ViewChild('stepper') stepper: MatStepper;
+  stepperOrientation: Observable<StepperOrientation>;
+  validationMessages = validationMessages;
   private _formBuilder = inject(FormBuilder);
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-
+  public firstFormGroup: FormGroup;
+  public secondFormGroup: FormGroup;
+  otpValue = ''
   hide = signal(true);
+  isResendButtonShow = false
+  constructor(private authService: AuthService,
+    private commonService: CommonService){
+
+  }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
+
+  goToNext(stepForm){
+    if (stepForm==1 && !this.firstFormGroup.invalid){
+      let reqVars = {
+        query: { email: this.firstFormGroup.controls['email'].value },
+        fields: { }
+      }
+      this.authService.apiRequest('post', 'users/forgotPassword', reqVars).subscribe(async response => {
+        if(response.status=='error'){
+          this.commonService.toastMessage(response?.data)
+        }else{
+          this.stepper.next();
+          setTimeout(() => {
+            this.isResendButtonShow = true;
+          }, 30000); // 30 seconds delay
+        }
+      })
+    }
+
+    if(stepForm==2){
+      let reqVars = {
+        query: { email: this.firstFormGroup.controls['email'].value, forgotPasswordOtp:this.otpValue },
+        fields: { }
+      }
+      this.authService.apiRequest('post', 'users/verifyOtp', reqVars).subscribe(async response => {
+        if(response.status=='error'){
+          this.commonService.toastMessage(response?.data)
+        }else{
+          this.stepper.next();
+        }
+      })
+    }
+
+    if (stepForm==1 && this.firstFormGroup.invalid){
+      this.firstFormGroup.markAllAsTouched();
+      return;
+    }
+  }
+
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   } 
-  onOtpChange(otp: string) { console.log('OTP changed:', otp); }
+
+  resendOtp(){
+    let reqVars = {
+      query: { email: this.firstFormGroup.controls['email'].value },
+      fields: { }
+    }
+    this.authService.apiRequest('post', 'users/resendOtp', reqVars).subscribe(async response => {
+      if(response.status=='error'){
+        this.commonService.toastMessage(response?.data)
+      }else{
+        this.isResendButtonShow = false;
+        this.commonService.toastMessage(response?.data.message)
+        setTimeout(() => {
+          this.isResendButtonShow = true;
+        }, 30000); // 30 seconds delay
+      }
+    })
+  }
+
+  onOtpChange(otp: string) { 
+    if(otp.length==6){
+      this.otpValue = otp
+    }
+  }
 }
